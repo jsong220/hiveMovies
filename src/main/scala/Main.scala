f9dbac32ae2ae61608505a8184289235f0f10ec9
Myung Jin Song <jsong220@gmail.com>
2021-01-31 19:35:18 -0800

    Refactor code to get rid of repeats

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,6 @@ import scala.io.StdIn.readLine
 
 object Main extends App {
   //println("Hello, World!")
-  //TODO: set up user interface..input and prompts
   def toInt(s: String): Option[Int] = {
     try {
       Some(s.toInt)
@@ -12,8 +11,37 @@ object Main extends App {
     }
   }
 
+  def sendQuery(initQuery: String, resultQuery: String, headerLabel: String, aggLabel: String, dropQuery: String, connection: Connection) {
+    val statement = connection.createStatement()
+    var initSet = statement.execute(initQuery)
+    var resultSet = statement.executeQuery(resultQuery)
+
+    //grabbing all the columns
+    while (resultSet.next) {
+      val header = resultSet.getString(headerLabel)
+      val aggValue = resultSet.getString(aggLabel)
+      println(header + " " + aggValue)
+    }
+    var endSet = statement.execute(dropQuery)
+  }
+
   var contProg = true
   while (contProg) {
+    //start connection to Hive
+    //catch error if connection was not made
+    var con: Connection = null
+    try {
+      val connectionString = "jdbc:hive2://localhost:10000/movies"
+      Class.forName("org.apache.hive.jdbc.HiveDriver")
+      con = DriverManager.getConnection(connectionString, "", "")
+    } catch {
+      case e: 
+        Exception => e.printStackTrace()
+        println("Failed to connect to Hive!")
+        sys.exit()
+    }
+    
+    //if Hive connection is established correctly..start with intro text
     println("")
     println("Please select a option for analysis..")
     printf(
@@ -21,6 +49,7 @@ object Main extends App {
     )
     var userOption = readLine()
 
+    //get valid userinput
     while (
       (toInt(userOption)
         .getOrElse(0) >= 5) || (toInt(userOption).getOrElse(0) <= 0)
@@ -31,136 +60,32 @@ object Main extends App {
 
     userOption.toInt match {
       case 1 =>
-        var con: Connection = null
-        try {
-          //set up connection
-          // val connectionString = "jdbc:hive2://localhost:10000/default"
-          //if not using default database
-          val connectionString = "jdbc:hive2://localhost:10000/movies"
-          Class.forName("org.apache.hive.jdbc.HiveDriver")
-          con = DriverManager.getConnection(connectionString, "", "")
-
-          //send query
-          val statement = con.createStatement()
-          var initSet = statement.execute(
-            "CREATE VIEW IF NOT EXISTS topMovieIds AS SELECT movieId, count(movieid) as ratingCount FROM ratings GROUP BY movieId ORDER BY ratingCount DESC"
-          )
-          var resultSet = statement.executeQuery(
-            "SELECT n.title, ratingCount FROM topMovieIds t JOIN movies n ON t.movieId = n.movieId LIMIT 10"
-          )
-
-          //grabbing all the columns
-          while (resultSet.next) {
-            val title = resultSet.getString("n.title")
-            val ratingCount = resultSet.getString("ratingCount")
-            println(title + " " + ratingCount)
-          }
-          var endSet = statement.execute("DROP VIEW topmovieids")
-
-        } catch {
-          case e: Exception => e.printStackTrace()
-        }
-        con.close()
+        println(" ")
+        println("Top 10 Most Rated Movies with rating counts")
+        sendQuery("CREATE VIEW IF NOT EXISTS topMovieIds AS SELECT movieId, count(movieid) as ratingCount FROM ratings GROUP BY movieId ORDER BY ratingCount DESC", "SELECT n.title, ratingCount FROM topMovieIds t JOIN movies n ON t.movieId = n.movieId LIMIT 10", "n.title", "ratingCount", "DROP VIEW topmovieids", con)
       case 2 =>
-        var con: Connection = null
-        try {
-          //set up connection
-          // val connectionString = "jdbc:hive2://localhost:10000/default"
-          //if not using default database
-          val connectionString = "jdbc:hive2://localhost:10000/movies"
-          Class.forName("org.apache.hive.jdbc.HiveDriver")
-          con = DriverManager.getConnection(connectionString, "", "")
-
-          //send query
-          val statement = con.createStatement()
-          var initSet = statement.execute(
-            "CREATE VIEW IF NOT EXISTS avgRatings AS SELECT movieId, avg(rating) as avgRating, COUNT(movieId) as ratingCount FROM ratings GROUP BY movieId ORDER BY avgRating DESC"
-          )
-          var resultSet = statement.executeQuery(
-            "SELECT n.title as movie_title, cast(avgRating AS DECIMAL(10,2)) AS avg_rating FROM avgRatings t JOIN movies n ON t.movieid = n.movieId WHERE ratingCount > 10 LIMIT 10"
-          )
-
-          //grabbing all the columns
-          while (resultSet.next) {
-            val movieTitle = resultSet.getString("movie_title")
-            val avgRating = resultSet.getString("avg_rating")
-            println(movieTitle + " " + avgRating)
-          }
-          var endSet = statement.execute("DROP VIEW avgRatings")
-
-        } catch {
-          case e: Exception => e.printStackTrace()
-        }
-        con.close()
+        println(" ")
+        println("Top 10 Highest Rated Movies")
+        sendQuery("CREATE VIEW IF NOT EXISTS avgRatings AS SELECT movieId, avg(rating) as avgRating, COUNT(movieId) as ratingCount FROM ratings GROUP BY movieId ORDER BY avgRating DESC","SELECT n.title as movie_title, cast(avgRating AS DECIMAL(10,2)) AS avg_rating FROM avgRatings t JOIN movies n ON t.movieid = n.movieId WHERE ratingCount > 10 LIMIT 10","movie_title","avg_rating","DROP VIEW avgRatings", con)
       case 3 =>
-        var con: Connection = null
-        try {
-          //set up connection
-          // val connectionString = "jdbc:hive2://localhost:10000/default"
-          //if not using default database
-          val connectionString = "jdbc:hive2://localhost:10000/movies"
-          Class.forName("org.apache.hive.jdbc.HiveDriver")
-          con = DriverManager.getConnection(connectionString, "", "")
+        println(" ")
+        println("Top 10 Highest Rated Movies by Different Decades")
+        println("From 1995 To 2000")
+        sendQuery("CREATE VIEW IF NOT EXISTS avgRatings AS SELECT movieid,avg(rating) AS avgRating, COUNT(movieId) as ratingCount FROM ratings WHERE t < 946684800 GROUP BY movieid ORDER BY avgRating DESC", "SELECT n.title AS movie_title, cast(avgRating AS DECIMAL(10,2)) AS avg_rating FROM avgRatings t JOIN movies n ON t.movieid = n.movieId WHERE ratingCount > 100 LIMIT 10", "movie_title", "avg_rating", "DROP VIEW avgRatings", con)
 
-          //send query
-          val statement = con.createStatement()
-          var initSet = statement.execute(
-            "CREATE VIEW IF NOT EXISTS avgRatings AS SELECT movieid,avg(rating) AS avgRating, COUNT(movieId) as ratingCount FROM ratings WHERE t < 946684800 GROUP BY movieid ORDER BY avgRating DESC"
-          )
-          var resultSet = statement.executeQuery(
-            "SELECT n.title AS movie_title, cast(avgRating AS DECIMAL(10,2)) AS avg_rating FROM avgRatings t JOIN movies n ON t.movieid = n.movieId WHERE ratingCount > 100 LIMIT 10"
-          )
-          //1995 to 2000
-          println("From 1995 To 2000")
-          //grabbing all the columns
-          while (resultSet.next) {
-            val movieTitle = resultSet.getString("movie_title")
-            val avgRating = resultSet.getString("avg_rating")
-            println(movieTitle + " " + avgRating)
-          }
-          var endSet = statement.execute("DROP VIEW avgRatings")
+        println(" ")
+        println("From 2000 To 2010")
+        sendQuery("CREATE VIEW IF NOT EXISTS avgRatings AS SELECT movieid,avg(rating) AS avgRating, COUNT(movieId) as ratingCount FROM ratings WHERE t < 1262304000 GROUP BY movieid ORDER BY avgRating DESC", "SELECT n.title AS movie_title, cast(avgRating AS DECIMAL(10,2)) AS avg_rating FROM avgRatings t JOIN movies n ON t.movieid = n.movieId WHERE ratingCount > 100 LIMIT 10", "movie_title", "avg_rating", "DROP VIEW avgRatings", con)
 
-          //2000 to 2010
-          initSet = statement.execute(
-            "CREATE VIEW IF NOT EXISTS avgRatings AS SELECT movieid,avg(rating) AS avgRating, COUNT(movieId) as ratingCount FROM ratings WHERE t < 1262304000 GROUP BY movieid ORDER BY avgRating DESC"
-          )
-          resultSet = statement.executeQuery(
-            "SELECT n.title AS movie_title, cast(avgRating AS DECIMAL(10,2)) AS avg_rating FROM avgRatings t JOIN movies n ON t.movieid = n.movieId WHERE ratingCount > 100 LIMIT 10"
-          )
-          println(" ")
-          println("From 2000 To 2010")
-          //grabbing all the columns
-          while (resultSet.next) {
-            val movieTitle = resultSet.getString("movie_title")
-            val avgRating = resultSet.getString("avg_rating")
-            println(movieTitle + " " + avgRating)
-          }
-          endSet = statement.execute("DROP VIEW avgRatings")
+        println(" ")
+        println("From 2010 To 2018")
+        sendQuery("CREATE VIEW IF NOT EXISTS avgRatings AS SELECT movieid,avg(rating) AS avgRating, COUNT(movieId) as ratingCount FROM ratings WHERE t < 1577836800 GROUP BY movieid ORDER BY avgRating DESC", "SELECT n.title AS movie_title, cast(avgRating AS DECIMAL(10,2)) AS avg_rating FROM avgRatings t JOIN movies n ON t.movieid = n.movieId WHERE ratingCount > 100 LIMIT 10", "movie_title", "avg_rating", "DROP VIEW avgRatings", con)
 
-          //2010 to 2020
-          initSet = statement.execute(
-            "CREATE VIEW IF NOT EXISTS avgRatings AS SELECT movieid,avg(rating) AS avgRating, COUNT(movieId) as ratingCount FROM ratings WHERE t < 1577836800 GROUP BY movieid ORDER BY avgRating DESC"
-          )
-          resultSet = statement.executeQuery(
-            "SELECT n.title AS movie_title, cast(avgRating AS DECIMAL(10,2)) AS avg_rating FROM avgRatings t JOIN movies n ON t.movieid = n.movieId WHERE ratingCount > 100 LIMIT 10"
-          )
-          println(" ")
-          println("From 2010 To 2018")
-          //grabbing all the columns
-          while (resultSet.next) {
-            val movieTitle = resultSet.getString("movie_title")
-            val avgRating = resultSet.getString("avg_rating")
-            println(movieTitle + " " + avgRating)
-          }
-          endSet = statement.execute("DROP VIEW avgRatings")
-
-        } catch {
-          case e: Exception => e.printStackTrace()
-        }
+      case 4 =>
+        //close connection when exiting
         con.close()
-      case 4 => contProg = false;
+        contProg = false;
       case _ => println("you should not be here..")
     }
   }
 }
-
